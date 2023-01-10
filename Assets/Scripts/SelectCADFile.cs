36278b10f16808eb1a49c2266fd64d09cc2a4940
Brett Davis <brett.p.davis101@gmail.com>
2023-01-09 20:50:13 -0600

    Base functionality
    Only works on blender .obj files. At this point, would require importing a CAD file (dwg, dxf, etc) into blender, lowering vertices if necessary, and exporting as .obj before importing into unity. Will try to circumvent this

---
+++ Assets/Scripts/SelectCADFile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.UI;
+using UnityEditor;
+using Dummiesman;
+
+public class SelectCADFile : MonoBehaviour
+{
+    [Button("Load CAD File")] private void Method1() { LoadCADFile(); }
+
+    string path;
+
+    void LoadCADFile()
+    {
+        if (!Application.isPlaying) {
+            Debug.LogWarning("Application must be running to load CAD files");
+            return;
+        }
+        //System.Diagnostics.Process.Start("explorer.exe");
+        path = EditorUtility.OpenFilePanel("Select .obj File", "", "obj");
+        GameObject importedObj = new OBJLoader().Load(path);
+        //Mesh importedMesh = GetComponent<ObjFromFile>().ImportFile(path);
+    }
+}
